4cef0b7e339b2ce041b6352e632dc25941ae287d
Varun Khalate <khalatevarun@gmail.com>
2021-01-22 12:29:12 +0530

    Create RecyclerView with Measured Views

--- app/src/main/java/com/example/android/mymemory/MainActivity.kt
+++ app/src/main/java/com/example/android/mymemory/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.mymemory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,11 @@ class MainActivity : AppCompatActivity() {
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
+
+        rvBoard.adapter = MemoryBoardAdapter(this,8)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, 2)
+
 
 
 
